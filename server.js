bd79c09c348a584a1ed076ce6a40d1bbf081d050
MichaTrojek <MichalTrojek1@gmail.com>
2021-01-05 21:04:29 +0100

    fixed cors

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+const cors = require('cors')({ origin: true });
 const {
   sendEmailOrderSentTo,
   sendEmailOrderConfirmedTo,
