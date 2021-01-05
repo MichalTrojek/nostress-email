69d3563ee6ccba70b6ccaa6c271bb45f609b19ef
MichaTrojek <MichalTrojek1@gmail.com>
2021-01-05 21:13:54 +0100

    test

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors')({ origin: true });
+
 const {
   sendEmailOrderSentTo,
   sendEmailOrderConfirmedTo,
