4f6688f1771b6302bd2bd9de344291b3cf6156ad
MichaTrojek <MichalTrojek1@gmail.com>
2021-01-28 16:54:50 +0100

    added loop and pending

--- server.js
+++ server.js
@@ -33,10 +33,18 @@ app.post('/api/sendEmailOrderConfirmed', (req, res) => {
 app.post('/api/sendOrderFinishedEmail', (req, res) => {
   const minutes = 25;
   const delay = 60 * 1000 * minutes;
+  const isFinished = false;
   setTimeout(() => {
     sendOrderFinishedEmailTo(req.body.email, JSON.parse(req.body.order));
+    isFinished = true;
   }, delay);
-  res.send(req.body.order);
+
+  while (true) {
+    if (isFinished) {
+      res.send(req.body.order);
+      break;
+    }
+  }
 });
 
 const PORT = 3000;
